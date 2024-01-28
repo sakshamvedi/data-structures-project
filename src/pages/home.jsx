7e6d2e88cf1d8916e8bd6145657601f8b134089d
Your Name <sakshamvedi@gmail.com>
2024-01-28 22:05:08 +0530

    ui fixes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Components/Header'
-import Image from '../resources/headerbg.png'
+import Image from '../resources/codeblock-bg-removed.png'
 import "../pageStyle/home.scss"
 function Home() {
     return (
@@ -8,8 +8,8 @@ function Home() {
             <Header />
             <div className="main-body">
                 <div className="left-side-description">
-                    <h1>Learn Things that Actually Matters </h1>
-                    <p>With Great Explaination , A great Illustration and Insightful Roadmaps</p>
+                    <h1>Learn Things That Actually Matters </h1>
+                    <p>With Great Explaination , A Great Illustration And Insightful Roadmaps</p>
                     <button>Start Learning</button>
                 </div>
                 <div className="right-side-description">
